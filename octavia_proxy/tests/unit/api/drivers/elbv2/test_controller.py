0dd452e82b305065acbf25fe868ca0c5de41be3a
Polina-Gubina <Polina.Gubina@t-systems.com>
2021-09-20 14:28:52 +0300

    test member

--- octavia_proxy/tests/unit/api/drivers/elbv2/test_controller.py
+++ octavia_proxy/tests/unit/api/drivers/elbv2/test_controller.py
@@ -2,7 +2,8 @@ from wsme import types as wtypes
 from octavia_proxy.api.v2.controllers import BaseV2Controller
 from octavia_proxy.api.v2.types import (load_balancer as lb_types,
                                         listener as listener_types,
-                                        pool as pool_types)
+                                        pool as pool_types,
+                                        member as member_types)
 from octavia_proxy.tests.unit import base
 
 
@@ -21,6 +22,10 @@ class TestElbv2Controler(base.TestCase):
     EXAMPLE_POOL = {'id': '70d638f5-29ba-443a-ba76-4277eb420292',
                     'name': 'ex_name', 'project_id': '7823987',
                     'provisioning_status': 'ACTIVE'}
+
+    EXAMPLE_MEMBER = {'id': '70d638f5-29ba-443a-ba76-4277eb420292',
+                      'name': 'ex_name', 'project_id': '7823987',
+                      'provisioning_status': 'ACTIVE'}
 
     LB_RESPONSE_TYPE_PROPERTIES = ['id', 'name', 'description',
                                    'provisioning_status', 'operating_status',
@@ -67,6 +72,14 @@ class TestElbv2Controler(base.TestCase):
                                      'tls_enabled', 'tls_ciphers',
                                      'tls_versions', 'alpn_protocols']
 
+    MEMBER_RESPONSE_TYPE_PROPERTIES = ['id', 'name', 'operating_status',
+                                       'provisioning_status', 'admin_state_up',
+                                       'address', 'protocol_port', 'weight',
+                                       'backup', 'subnet_id', 'project_id',
+                                       'created_at', 'updated_at',
+                                       'monitor_address', 'monitor_port',
+                                       'tags']
+
     def setUp(self):
         super().setUp()
         self.controller = BaseV2Controller()
@@ -75,6 +88,8 @@ class TestElbv2Controler(base.TestCase):
             **self.EXAMPLE_LSNR)
         self.pool_response = pool_types.PoolResponse(
             **self.EXAMPLE_POOL)
+        self.member_response = member_types.MemberResponse(
+            **self.EXAMPLE_MEMBER)
 
     def _assert_only_filtered_fields_present(self, list_objects, fields,
                                              type_properties):
@@ -93,14 +108,21 @@ class TestElbv2Controler(base.TestCase):
 
     def test_filter_fields_listener(self):
         lsnr_objects = [self.lsnr_response]
-        fields = ['id', 'name', 'provisioning_status']
+        fields = ['id', 'name']
         result = self.controller._filter_fields(lsnr_objects, fields)
         self._assert_only_filtered_fields_present(
             result, fields, self.LSNR_RESPONSE_TYPE_PROPERTIES)
 
     def test_filter_fields_pool(self):
         pool_objects = [self.pool_response]
-        fields = ['id', 'name', 'provisioning_status']
+        fields = ['id', 'name']
         result = self.controller._filter_fields(pool_objects, fields)
         self._assert_only_filtered_fields_present(
             result, fields, self.POOL_RESPONSE_TYPE_PROPERTIES)
+
+    def test_filter_fields_member(self):
+        member_objects = [self.member_response]
+        fields = ['id', 'name']
+        result = self.controller._filter_fields(member_objects, fields)
+        self._assert_only_filtered_fields_present(
+            result, fields, self.MEMBER_RESPONSE_TYPE_PROPERTIES)
