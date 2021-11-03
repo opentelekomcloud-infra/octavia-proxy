30acb01f3d6e0fee145ad7aad58b05ec44417778
Irina Pereiaslavskaia <irina.pereyaslavskaya@gmail.com>
2021-11-03 13:50:41 +0300

    Add pagination for health_monitor.

--- octavia_proxy/api/v2/controllers/health_monitor.py
+++ octavia_proxy/api/v2/controllers/health_monitor.py
@@ -24,6 +24,7 @@ from octavia_proxy.api.drivers import driver_factory
 from octavia_proxy.api.drivers import utils as driver_utils
 from octavia_proxy.api.v2.controllers import base
 from octavia_proxy.api.v2.types import health_monitor as hm_types
+from octavia_proxy.api.common import types
 from octavia_proxy.common import constants as const
 from octavia_proxy.common import exceptions
 from octavia_proxy.i18n import _
@@ -64,16 +65,25 @@ class HealthMonitorController(base.BaseController):
         context = pcontext.get('octavia_context')
 
         query_filter = self._auth_get_all(context, project_id)
-        query_params = pcontext.get(const.PAGINATION_HELPER).params
-
-        query_filter.update(query_params)
-
+        pagination_helper = pcontext.get(const.PAGINATION_HELPER)
+        #query_params = pagination_helper.params
+        #query_filter.update(query_params)
         is_parallel = query_filter.pop('is_parallel', True)
-        links = []
 
+        links = []
         result = driver_invocation(
             context, 'health_monitors', is_parallel, query_filter
         )
+
+        if pagination_helper:
+            result_to_dict = [hlth_mntr.to_dict() for hlth_mntr in result]
+            temp_result, temp_links = pagination_helper.apply(result_to_dict)
+            LOG.debug('RESULT {temp_result}'.format(temp_result = temp_result))
+            LOG.debug('LINKS {temp_links}'.format(temp_links = temp_links))
+            links = [types.PageType(**link) for link in temp_links]
+            result = self._convert_sdk_to_type(
+                temp_result, hm_types.HealthMonitorFullResponse
+            )
 
         if fields is not None:
             result = self._filter_fields(result, fields)
