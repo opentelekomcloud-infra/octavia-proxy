a0f9d726ede94a0f5f1590e1a147f7f4e82dc185
Irina Pereiaslavskaia <irina.pereyaslavskaya@gmail.com>
2021-11-03 13:44:55 +0300

    Add pagination for flavors.

--- octavia_proxy/api/v2/controllers/flavors.py
+++ octavia_proxy/api/v2/controllers/flavors.py
@@ -24,6 +24,7 @@ from wsmeext import pecan as wsme_pecan
 from octavia_proxy.api.common.invocation import driver_invocation
 from octavia_proxy.api.v2.controllers import base
 from octavia_proxy.api.v2.types import flavors as flavor_types
+from octavia_proxy.api.common import types
 from octavia_proxy.common import constants
 
 CONF = cfg.CONF
@@ -59,14 +60,25 @@ class FlavorsController(base.BaseController):
         context = pcontext.get('octavia_context')
 
         query_filter = self._auth_get_all(context, project_id)
-        query_params = pcontext.get(constants.PAGINATION_HELPER).params
-        query_filter.update(query_params)
+        pagination_helper = pcontext.get(constants.PAGINATION_HELPER)
+        # query_params = pagination_helper.params
+        # query_filter.update(query_params)
         is_parallel = query_filter.pop('is_parallel', True)
 
         links = []
         result = driver_invocation(
             context, 'flavors', is_parallel, query_filter
         )
+
+        if pagination_helper:
+            result_to_dict = [flvr_obj.to_dict() for flvr_obj in result]
+            temp_result, temp_links = pagination_helper.apply(result_to_dict)
+            LOG.debug('RESULT {temp_result}'.format(temp_result = temp_result))
+            LOG.debug('LINKS {temp_links}'.format(temp_links = temp_links))
+            links = [types.PageType(**link) for link in temp_links]
+            result = self._convert_sdk_to_type(
+                temp_result, flavor_types.FlavorRootResponse
+            )
 
         if fields is not None:
             result = self._filter_fields(result, fields)
