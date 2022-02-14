9385325c725d1dba10751cf6da3a15b7e53da762
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2022-02-14 14:23:59 +0300

    fix pep

--- octavia_proxy/api/drivers/elbv2/driver.py
+++ octavia_proxy/api/drivers/elbv2/driver.py
@@ -604,7 +604,8 @@ class ELBv2Driver(driver_base.ProviderDriver):
         )
 
     def l7rules(
-            self, session, project_id, l7policy_id, query_filter=None, **kwargs):
+            self, session, project_id,
+            l7policy_id, query_filter=None, **kwargs):
         LOG.debug('Fetching l7 rules')
 
         result = []
